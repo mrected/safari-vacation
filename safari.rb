c935ee04465e8f626564d9ea373580f85873d6ea
Chris Terrazas <chris.terrazas@gmail.com>
2019-01-01 19:57:21 -0500

    Searches by location, increments count_of_times_seen by 1

--- safari.rb
+++ safari.rb
@@ -12,20 +12,49 @@ ActiveRecord::Base.establish_connection(
 class SeenAnimal < ActiveRecord::Base
 end 
 
+
+# returns all animals
 get '/Animals' do 
-    # return all_the_animals as json
     json SeenAnimal.all
 end
- 
+
+# return any animals where params match anything in species
 get '/Search' do
-    # return any animals where params match anything in species
     json SeenAnimal.where('species ILIKE ?', "%#{params["species"]}%")
 end
 
+# adds an animal via 
 post '/Animal' do
+
+    # {
+    #     "seen_animal":{
+    #         "species": "dog",
+    #         "count_of_times_seen":2,
+    #         "last_location_seen": "playing poker"
+    #     }
+    # }
+
     animal_json = JSON.parse(request.body.read)
 
     animal_active_record = SeenAnimal.create(animal_json["seen_animal"])
 
     json animal_active_record
+end
+
+# gets animals only by EXACT location
+get '/Animal/:location' do
+
+    json SeenAnimal.where(last_location_seen: params["location"])
+
+end
+
+# adds 1 to count of times seen for given animal id
+put '/Animal/:id' do
+
+    found_animal = SeenAnimal.find(params["id"])
+
+    found_animal.update(count_of_times_seen: found_animal.count_of_times_seen + 1)
+
+    json found_animal
+
 end
